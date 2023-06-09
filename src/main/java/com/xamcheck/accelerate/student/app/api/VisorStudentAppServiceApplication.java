a914e6bd50df2efacf6b942b53f7b054eb500d0d
nigashish <ashishn@eupheus.in>
2023-06-09 16:33:41 +0530

    add postgres db dependency

--- src/main/java/com/xamcheck/accelerate/student/app/api/VisorStudentAppServiceApplication.java
+++ src/main/java/com/xamcheck/accelerate/student/app/api/VisorStudentAppServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
 @SpringBootApplication
 @EnableEurekaClient
 @EntityScan(basePackages = {"com.accelerate.visor.model"})
